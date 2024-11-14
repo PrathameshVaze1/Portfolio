b9da4446b34a87fb020b2b48dd45aa44e30e27f8
Prathamesh <prathameshvaze@gmail.com>
2024-11-14 23:14:42 +0530

    Add Livewire And Start Learning

--- app/Livewire/Greeter.php
+++ app/Livewire/Greeter.php
@@ -8,7 +8,8 @@ class Greeter extends Component
 {
     public $name = 'Prathamesh';
 
-    public function change($newName){
+    public function change($newName)
+    {
         $this->name = $newName;
     }
 
